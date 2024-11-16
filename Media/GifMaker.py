b320e99cad8a70e5d7642a32676a875b233d289c
EliasDeCoppel <elias.decoppel@gmail.com>
2024-11-16 15:42:47 +0100

    Update README

--- Media/GifMaker.py
+++ Media/GifMaker.py
@@ -1,4 +1,4 @@
 from moviepy.editor import VideoFileClip
 
 clip = VideoFileClip("Media/TinyShootersCompilation.mp4")
-clip.write_gif("Media/TinyShootersCompilation.gif", 10)
+clip.write_gif("Media/TinyShootersCompilation.gif", 5)
